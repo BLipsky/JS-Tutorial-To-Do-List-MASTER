9f90e49e04c24e140bcb9118d48b48cb26169ab8
Ben_Lipsky <Benlipsky@icloud.com>
2024-05-15 13:48:40 -0400

    cd

--- js/app.js
+++ js/app.js
@@ -1,1 +1,55 @@
 // CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+let LIST, id;
+
+const options = {weekday : "long", month:"shot", day:"numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+
+
+function addToDo(toDo, id, done, trash){
+
+    if(trash){ return; }
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const text = `<li class="item">
+                    <i class="co fa fa-circle-thin" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}"> ${toDo} </p>
+                    <i class="de fa fa-trash-o" job="delete" id="${id}"></i>
+                  </li>`
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, text);
+}
+addToDo("Drink Coffee");
+
+document.addEventListener("keyup", function(even){
+    if(event.keyCode == 13){
+        const toDo = input.value;
+        if(toDo){
+            addToDo(toDo, id, false, false);
+
+            LIST.push({
+                name : toDo,
+                id : id,
+                done : false,
+                trash : false
+            });
+            id++;
+        }
+        input.value = "";
+    }
+});
+
+addToDo("Coffee", 1, false, true);
