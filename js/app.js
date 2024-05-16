40e4889388adb161ee4a2d70f0349a22bb83722a
Ben_Lipsky <Benlipsky@icloud.com>
2024-05-16 14:16:40 -0400

    I want to commit after this

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// CODE EXPLAINED channel
 const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
@@ -10,46 +9,95 @@ const LINE_THROUGH = "lineThrough";
 
 let LIST, id;
 
-const options = {weekday : "long", month:"shot", day:"numeric"};
+let data = localStorage.getItem("TODO");
+
+if (data) {
+    LIST = JSON.parse(data);
+    id = LIST.length; // set the id to the last one in the list
+    loadList(LIST); // load the list to the user interface
+} else {
+    LIST = [];
+    id = 0;
+}
+
+function loadList(array) {
+    array.forEach(function(item) {
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
+
+clear.addEventListener("click", function() {
+    localStorage.clear();
+    location.reload();
+});
+
+const options = { weekday: "long", month: "short", day: "numeric" };
 const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
-
-
-function addToDo(toDo, id, done, trash){
-
-    if(trash){ return; }
+function addToDo(toDo, id, done, trash) {
+    if (trash) { return; }
 
     const DONE = done ? CHECK : UNCHECK;
     const LINE = done ? LINE_THROUGH : "";
 
-    const text = `<li class="item">
-                    <i class="co fa fa-circle-thin" job="complete" id="${id}"></i>
-                    <p class="text ${LINE}"> ${toDo} </p>
-                    <i class="de fa fa-trash-o" job="delete" id="${id}"></i>
-                  </li>`
-    const position = "beforeend";
-    list.insertAdjacentHTML(position, text);
-}
-addToDo("Drink Coffee");
+    const item = `<li class="item">
+                    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                  </li>`;
 
-document.addEventListener("keyup", function(even){
-    if(event.keyCode == 13){
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
+
+document.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
         const toDo = input.value;
-        if(toDo){
+
+        if (toDo) {
             addToDo(toDo, id, false, false);
 
             LIST.push({
-                name : toDo,
-                id : id,
-                done : false,
-                trash : false
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false
             });
+
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+
             id++;
         }
         input.value = "";
     }
 });
 
-addToDo("Coffee", 1, false, true);
+function completeToDo(element) {
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+    LIST[element.id].done = !LIST[element.id].done;
+}
+
+function removeToDo(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+}
+
+list.addEventListener("click", function(event) {
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+
+    if (elementJob === "complete") {
+        completeToDo(element);
+    } else if (elementJob === "delete") {
+        removeToDo(element);
+    }
+
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
